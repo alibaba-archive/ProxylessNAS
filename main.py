e16885a127f3e436f281e6887f2a6d6d44073eb4
ChengTsang <zc0702@outlook.com>
2019-07-17 17:16:26 +0800

    fix bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # import ray
 import argparse
 import time
+import math
 
 import torch
 import torch.nn as nn
@@ -10,7 +11,6 @@ import torch.optim
 import torch.utils.data
 import torchvision
 
-from architecture_trainer import ValAcc
 from weight_trainer import SampleNet
 from blocks_cifar import get_blocks
 
@@ -86,6 +86,7 @@ def main():
     model = SampleNet(num_classes=10, blocks=blocks)
     ### model = torch.nn.DataParallel(model, device_ids=[0] if args.single_gpu else None).cuda()
     model.architecture_parameters = read_architecture_parameters()
+    architecture_parameters = model.architecture_parameters
     ### NOTE
     # criterion = nn.CrossEntropyLoss().cuda()
     criterion = nn.CrossEntropyLoss()
@@ -100,7 +101,7 @@ def main():
         # train weight for one epoch
         model.is_train_weight = True
         print('train_weight_parameters')
-        train_weight_parameters(train_loader, optimizer, model, criterion, epoch)
+        # train_weight_parameters(train_loader, optimizer, model, criterion, epoch)
 
         # Configure 20 workers and get acc by interacting with the environment
         index_candidate_blocks = [get_index_candidate_blocks(architecture_parameters) for _ in range(20)]
@@ -109,8 +110,11 @@ def main():
         for i in range(20):
             model.index_candidate_block = index_candidate_blocks[i]
             models.append(model)
-        val_accs = [ValAcc.remote(val_loader, models[i]) for i in range(20)]
-        acc_results = ray.get([v.get_acc.remote() for v in val_accs])
+        ### val_accs = [ValAcc.remote(val_loader, models[i]) for i in range(20)]
+        val_accs = [ValAcc(models[i]) for i in range(20)]
+
+        ### acc_results = ray.get([v.get_acc.remote() for v in val_accs])
+        acc_results = [v.get_acc() for v in val_accs]
         print('train_architecture_parameters')
         architecture_parameters = train_architecture_parameters(architecture_parameters, index_candidate_blocks, acc_results)
 
@@ -138,14 +142,17 @@ def adjust_learning_rate(optimizer, epoch):
 def train_architecture_parameters(architecture_parameters, index_candidate_blocks, acc_results):
     M = len(acc_results)
     for layer in range(len(architecture_parameters)):
-        sum_layer = sum([exp(i) for i in architecture_parameters[layer]])
+        sum_layer = sum([math.exp(i) for i in architecture_parameters[layer]])
         for op in range(len(architecture_parameters[layer])):
             grad = 0
             for m in range(M):
                 if op == index_candidate_blocks[m][layer]:
-                    grad += 1/M * acc_results[m] * (1 - exp(architecture_parameters[layer][op])/sum_layer)
+
+                    grad += 1/M * acc_results[m] * (1 - math.exp(architecture_parameters[layer][op])/sum_layer)
                 else:
-                    grad += 1/M * acc_results[m] * (-1 * exp(architecture_parameters[layer][op])/sum_layer)
+                    print(11111111111111111)
+                    print(math.exp(architecture_parameters[layer][op]))
+                    grad += 1/M * acc_results[m] * (-1 * math.exp(architecture_parameters[layer][op])/sum_layer)
             architecture_parameters[layer][op] -= grad
 
     return architecture_parameters
@@ -224,6 +231,68 @@ def accuracy(output, target):
     pred = torch.argmax(output, dim=1)
     acc = torch.sum(pred == target).float() / batch_size
     return acc
+
+### @ray.remote
+class ValAcc(object):
+    def __init__(self, model):
+        self.model = model
+
+    def get_acc(self):
+        normalize = transforms.Normalize(mean=[0.4914, 0.4822, 0.4465],
+                                         std=[0.2473, 0.2434, 0.2610])
+        transform_val = transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ])
+        valset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                              download=True, transform=transform_val)
+        val_loader = torch.utils.data.DataLoader(
+            valset,
+            batch_size=args.batch_size, shuffle=False,
+            num_workers=args.workers, pin_memory=True)
+        criterion = nn.CrossEntropyLoss()
+        # criterion = nn.CrossEntropyLoss().cuda()
+
+        batch_time = AverageMeter()
+        losses = AverageMeter()
+        top1 = AverageMeter()
+        top5 = AverageMeter()
+
+        self.model.eval()
+        end = time.time()
+
+        for i, (input, target) in enumerate(val_loader):
+
+            ### target = target.cuda(async=True)
+            input_var = torch.autograd.Variable(input, volatile=True)
+            target_var = torch.autograd.Variable(target, volatile=True)
+
+            # compute output
+            output = self.model(input_var)
+            loss = criterion(output, target_var).sum()
+
+            # measure accuracy and record loss
+            prec1 = accuracy(output.data, target)
+            losses.update(loss.data.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+            # top5.update(prec5.item(), input.size(0))
+
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+
+            if i % args.print_freq == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                    i, len(val_loader), batch_time=batch_time, loss=losses,
+                    top1=top1))
+
+        print(' * Prec@1 {top1.avg:.3f}'
+              .format(top1=top1))
+
+        return top1.avg
 
 if __name__ == '__main__':
     main()
