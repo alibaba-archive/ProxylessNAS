09a8f37b600714fdf682f0681ee914903ae1773f
ChengTsang <34964022+ChengTsang@users.noreply.github.com>
2019-08-16 15:52:01 +0800

    support other data set

--- Code/CIFAR/models/utils.py
+++ Code/CIFAR/models/utils.py
@@ -114,3 +114,35 @@ class BasicUnit(nn.Module):
 
     def get_flops(self, x):
         raise NotImplementedError
+
+class NormalizeForSinglePicture(object):
+    """Normalize a tensor image with mean and standard deviation.
+    Given mean: ``(M1,...,Mn)`` and std: ``(S1,..,Sn)`` for ``n`` channels, this transform
+    will normalize each channel of the input ``torch.*Tensor`` i.e.
+    ``input[channel] = (input[channel] - mean[channel]) / std[channel]``
+
+    Args:
+        mean (sequence): Sequence of means for each channel.
+        std (sequence): Sequence of standard deviations for each channel.
+    """
+
+    def __init__(self):
+        pass
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+
+        Returns:
+            Tensor: Normalized Tensor image.
+        """
+        mean = []
+        std = []
+        for i in range(3):
+            mean.append(tensor[i].mean().item())
+            std.append(tensor[i].std().item())
+        return F.normalize(tensor, mean, std)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
